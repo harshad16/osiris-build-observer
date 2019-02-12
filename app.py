1a095c98ca021a85bc3f4561b627e056eb7373cf
Marek Cermak <macermak@redhat.com>
2019-02-12 16:27:43 +0100

    Modify app.py header
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-#!/bin/env python
-# Osiris: Build log aggregator.
+#!/urs/bin/env python3
+# osiris-build-observer: OpenShift build event observer.
+
+# Copyright(C) 2019 Marek Cermak
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Observer module.
 
-This module observer OpenShift namespace and watches for build events.
-When such event occur, it puts it to [Osiris](https://github.com/CermakM/osiris)
+This module observes OpenShift namespace and watches for build events.
+When such event occurs, it puts it to [Osiris](https://github.com/thoth-station/osiris)
 endpoint for further processing.
 """
 
