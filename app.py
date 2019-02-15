be40a8cb22dddc107ddc6499811a4458018633c5
Marek Cermak <macermak@redhat.com>
2019-02-15 17:42:25 +0100

    Use Thoth common OpenShift and loggin API
    - replace kubernetes client with OpenShift client
    - use ResourceInstance instead of kuberenetes V1Event (NOTE: This causes
    temporarily incompatibility with Osiris API)
    - do not require additional event filters (only get build resources)
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   Pipfile
    modified:   Pipfile.lock
    modified:   app.py
    modified:   openshift/deployment-template.yaml
    modified:   openshift/template.yaml

--- app.py
+++ app.py
@@ -26,14 +26,12 @@ endpoint for further processing.
 import os
 import re
 import sys
-import time
 import typing
 
 import requests
 import urllib3
 
 import logging
-import daiquiri.formatter
 
 from functools import reduce
 
@@ -43,78 +41,36 @@ from requests.adapters import HTTPAdapter
 from requests.adapters import Retry
 from urllib.parse import urljoin
 
-import kubernetes
-from kubernetes.client.models.v1_event import V1Event as Event
+from openshift.dynamic import ResourceInstance
+
+from thoth.common import OpenShift
+from thoth.common import init_logging
+
+
+init_logging()
+
+_LOGGER = logging.getLogger("thoth.osiris_build_observer")
 
 
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
-
-DEFAULT_LOGGING_FORMAT = (
-    "%(asctime)s [%(process)d] %(color)s%(levelname)-8.8s "
-    "%(name)s: %(dry_run_prefix)s%(app_prefix)s%(event_prefix)s %(message)s%(color_stop)s"
-)
-
-output_formatter = daiquiri.formatter.ColorFormatter(
-    fmt=DEFAULT_LOGGING_FORMAT
-)
-output_stream = daiquiri.output.Stream(formatter=output_formatter)
-
-daiquiri.setup(
-    level=getattr(logging, os.getenv('LOG_LEVEL', 'INFO'), logging.INFO),
-    outputs=[
-        output_stream
-    ]
-)
-
-_DRY_RUN = os.getenv("DRY_RUN", 'false') == 'true'
-_LOGGER = daiquiri.getLogger(
-    dry_run_prefix='[DRY_RUN] ' if _DRY_RUN else '', app_prefix='[OBSERVER]', event_prefix='[EVENT]'
-)
-
 
 # osiris configuration
 
 _OSIRIS_HOST_NAME = os.getenv("OSIRIS_HOST_NAME", "http://0.0.0.0")
 _OSIRIS_HOST_PORT = os.getenv("OSIRIS_HOST_PORT", "5000")
-_OSIRIS_LOGIN_ENDPOINT = "/auth/login"
 _OSIRIS_BUILD_START_HOOK = "/build/started/"
 _OSIRIS_BUILD_COMPLETED_HOOK = "/build/completed/"
 
-# oc namespace
+# openshift client
 
 _NAMESPACE_FILENAME = '/run/secrets/kubernetes.io/serviceaccount/namespace'
 
 try:
     _NAMESPACE = Path(_NAMESPACE_FILENAME).read_text()
 except FileNotFoundError:
-    _NAMESPACE = os.getenv("OC_NAMESPACE", None)
+    _NAMESPACE = os.getenv("MIDDLETIER_NAMESPACE", None)
 
-_REQUESTS_MAX_RETRIES = 10
-
-
-# kubernetes configuration
-
-_SERVICE_TOKEN_FILENAME = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-_SERVICE_CERT_FILENAME = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-
-if Path(_SERVICE_TOKEN_FILENAME).exists():  # in-cluster configuration
-
-    _KUBE_CONFIG = kubernetes.config.incluster_config.InClusterConfigLoader(
-        token_filename=_SERVICE_TOKEN_FILENAME, cert_filename=_SERVICE_CERT_FILENAME)
-    _KUBE_CONFIG.load_and_set()
-
-    _KUBE_CLIENT = kubernetes.client.CoreV1Api()
-    _KUBE_API = _KUBE_CLIENT.api_client
-
-else:  # default configuration, assumes current host logs in to the oc cluster by himself
-    _KUBE_CONFIG = kubernetes.client.Configuration()
-    _KUBE_CONFIG.host = os.getenv("OC_HOST_NAME", 'localhost')
-    _KUBE_CONFIG.verify_ssl = False
-
-    kubernetes.config.load_kube_config(client_configuration=_KUBE_CONFIG)
-
-    _KUBE_API = kubernetes.client.ApiClient(_KUBE_CONFIG)
-    _KUBE_CLIENT = kubernetes.client.CoreV1Api(_KUBE_API)
+_OPENSHIFT_CLIENT = OpenShift(middletier_namespace=_NAMESPACE)
 
 
 def noexcept(fun: typing.Callable):
@@ -143,6 +99,8 @@ class RetrySession(requests.Session):
     progressively prolonged for a certain amount of retries.
     """
 
+    _REQUESTS_MAX_RETRIES = 10
+
     def __init__(self,
                  adapter_prefixes: typing.List[str] = None,
                  status_forcelist: typing.Tuple[int] = (500, 502, 504),
@@ -153,8 +111,8 @@ class RetrySession(requests.Session):
         adapter_prefixes = adapter_prefixes or ["http://", "https://"]
 
         retry_config = Retry(
-            total=_REQUESTS_MAX_RETRIES,
-            connect=_REQUESTS_MAX_RETRIES,
+            total=self._REQUESTS_MAX_RETRIES,
+            connect=self._REQUESTS_MAX_RETRIES,
             backoff_factor=5,  # determines sleep time
             status_forcelist=status_forcelist,
             method_whitelist=method_whitelist
@@ -164,30 +122,31 @@ class RetrySession(requests.Session):
         for prefix in adapter_prefixes:
             self.mount(prefix, retry_adapter)
 
+    def send_request(self, request):
+        """Send request and wrap it in try-except block."""
+        try:
+            resp = self.send(request, timeout=60)
 
-@noexcept
-def _is_pod_event(event: Event) -> bool:
-    return event.involved_object.kind == 'Pod'
+        except urllib3.exceptions.MaxRetryError:
+            _LOGGER.error("Failure. Max retries exceeded. Skipping.")
 
+        else:
+            if resp.status_code == HTTPStatus.ACCEPTED:
 
-@noexcept
-def _is_build_event(event: Event) -> bool:
-    is_build = event.involved_object.kind == 'Build'
-    # check for [BuildStarted, BuildCompleted, BuildFailed, ...] events
-    is_valid = re.search(r'^Build', event.reason, flags=re.IGNORECASE)
+                _LOGGER.info("Success.")
+            else:
 
-    return is_build and is_valid
+                _LOGGER.info("Failure.")
+                _LOGGER.info("Status: %d  Reason: %r",
+                             resp.status_code, resp.reason)
 
-
-@noexcept
-def _is_observed_event(event: Event) -> bool:
-    # TODO: check for valid event names
-    return _is_build_event(event) or _is_pod_event(event)
+            _LOGGER.debug("Status: %d  Reason: %r  Response: %s",
+                          resp.status_code, resp.reason, resp.json)
 
 
 if __name__ == "__main__":
 
-    watch = kubernetes.watch.Watch()
+    v1_build = _OPENSHIFT_CLIENT.ocp_client.resources.get(api_version='v1', kind='Build')
 
     put_request = requests.Request(
         url=':'.join([_OSIRIS_HOST_NAME, _OSIRIS_HOST_PORT]),
@@ -196,74 +155,60 @@ if __name__ == "__main__":
             'content-type': 'application/json'
         },
         params={
-            'schema': 'event'
+            'mode': 'cluster'
         }
     )
 
     with RetrySession() as r3_session:
 
-        _LOGGER.info(f"Watching for events in {_NAMESPACE} namespace ...", event_prefix="")
+        _LOGGER.info(f"Watching for build events in {_NAMESPACE} namespace ...")
 
-        for streamed_event in watch.stream(_KUBE_CLIENT.list_namespaced_event,
-                                           namespace=_NAMESPACE):
+        for streamed_event in v1_build.watch(namespace=_NAMESPACE):
 
-            kube_event: Event = streamed_event['object']
-            kube_event_raw = streamed_event['raw_object']
-
-            if not _is_observed_event(kube_event):
-
-                time.sleep(5)  # there are probably no events atm so keep calm and observe
-                continue
+            kube_event: ResourceInstance = streamed_event['object']
+            kube_event_raw: dict = streamed_event['raw_object']
 
             _LOGGER.debug("New event received.")
-            _LOGGER.debug("Reason: %s", kube_event.reason)
-            _LOGGER.debug("Involved object: \n%s", kube_event.involved_object)
+            _LOGGER.debug("Kind: %s", kube_event.kind)
+            _LOGGER.debug("Config: \n%s", kube_event.status.config)
 
-            if _is_pod_event(kube_event):
-                continue
+            build_id = kube_event.metadata.name
+            build_complete = re.search(r"complete", kube_event.status.phase, re.IGNORECASE)
 
-            # get associated pod and use it as a build_id
-            if __name__ == '__main__':
-                build_url = urljoin(_KUBE_CLIENT.api_client.configuration.host,
-                                    kube_event.metadata.self_link)
+            build_log = {}
 
-            build_complete = not re.search(r"start", kube_event.reason, re.IGNORECASE)
-            osiris_endpoint = _OSIRIS_BUILD_COMPLETED_HOOK if build_complete else _OSIRIS_BUILD_START_HOOK
+            if build_complete:
 
-            put_request.url = reduce(urljoin, [put_request.url, osiris_endpoint, kube_event.involved_object.name])
+                build_log_data = _OPENSHIFT_CLIENT.get_build_log(  # TODO: can log level be modified?
+                    build_id=build_id,
+                    namespace=_NAMESPACE
+                )
+                build_log = {
+                    'data': build_log_data,
+                    'metadata': {
+                        # TODO: more useful metadata?
+                        'build_id': build_id
+                    }
+                }
+                osiris_endpoint = _OSIRIS_BUILD_COMPLETED_HOOK
+
+            else:
+                osiris_endpoint = _OSIRIS_BUILD_START_HOOK
+
+            put_request.url = reduce(urljoin, [put_request.url, osiris_endpoint, build_id])
             put_request.json = kube_event_raw
 
-            prep_request = r3_session.prepare_request(put_request)
-
-            _LOGGER.debug("Event to be posted: \n%r", kube_event_raw)
-            _LOGGER.debug("Request: %s", prep_request.body.decode('utf-8'))
+            build_info_request = r3_session.prepare_request(put_request)
 
             _LOGGER.info("Posting event '%s' to: %s", kube_event.kind, put_request.url)
 
-            if not _DRY_RUN:
+            r3_session.send_request(build_info_request)
 
-                try:
-                    resp = r3_session.send(prep_request, timeout=60)
+            if build_complete:
+                put_request.json = build_log
+                build_log_request = r3_session.prepare_request(put_request)
 
-                except urllib3.exceptions.MaxRetryError:
+                _LOGGER.info("Posting build log to: %s", put_request.url)
 
-                    _LOGGER.error("Failure. Max retries exceeded. Skipping.")
-
-                else:
-
-                    if resp.status_code == HTTPStatus.ACCEPTED:
-
-                        _LOGGER.info("Success.")
-
-                    else:
-
-                        _LOGGER.info("Failure.")
-                        _LOGGER.info("Status: %d  Reason: %r",
-                                     resp.status_code, resp.reason)
-
-                    _LOGGER.debug("Status: %d  Reason: %r  Response: %s",
-                                  resp.status_code, resp.reason, resp.json)
-
-            else:
-
-                _LOGGER.info("Finished.")
+                # TODO: Osiris does not allow to put build log and build info in one request
+                r3_session.send_request(build_log_request)
